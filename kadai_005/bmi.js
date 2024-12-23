f515d09fcc36c06c6547c87dfb7c0f87d9b5ee47
TKspyair <tkworking2003@gmail.com>
2024-12-23 10:21:33 +0900

    アップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+let bodyWeight;
+bodyWeight = 68 + 'kg';
+console.log(bodyWeight);
+
+let bodyHeight;
+bodyHeight = 1.7 + 'm';
+console.log(bodyHeight);
+
+let Result;
+Result =  68 / (1.7 * 1.7);
+console.log(Result);
+
+
