c995f706af8097379a99901ab28dfcafb9161049
TKspyair <tkworking2003@gmail.com>
2025-01-11 16:55:36 +0900

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,8 +0,0 @@
-const newText = "ボタンをクリックしました";
-const btn = document.getElementById('btn');
-const text = document.getElementById('text');
-setTimeout(() => {
-    btn.addEventListener('click', () =>{
-    text.textContent = newText;
-});
-}, 2000);
